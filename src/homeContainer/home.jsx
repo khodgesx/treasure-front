87dfce160c75562d5569d6951dfb98f7d2fa3e96
khodges <zkellyhodges@gmail.com>
2022-04-12 08:07:25 -0700

    added photo to homepage and edited styling there

--- src/homeContainer/home.jsx
+++ src/homeContainer/home.jsx
@@ -1,8 +1,14 @@
+import '../App.css'
+
 const Home = () =>{
     return(
-    <div>
-        <h1>Treasure Trash</h1>
-        <h2>Where trash is treasure or treasure is trash</h2>
+    <div id="home">
+        <div>
+            <h1>Treasure Trash</h1>
+            <h2>Where trash is treasure or treasure is trash</h2>
+        </div>
+        
+        <img src="https://i.imgur.com/dCbVGOK.png"></img>
     </div>
     )  
 }
