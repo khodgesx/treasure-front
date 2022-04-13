cb77388442a3fb427c9673c9bb604fd535a6cf90
khodges <zkellyhodges@gmail.com>
2022-04-13 12:52:24 -0700

    fixed edit photo route - worth refactoring in the future

--- src/itemsContainer/editItemContainer/editItemRoute.jsx
+++ src/itemsContainer/editItemContainer/editItemRoute.jsx
@@ -15,6 +15,7 @@ const EditItemRoute = (props)=>{
     }, [])
     const [show, setShow] = useState(false)
     const toggleShow=()=>setShow(!show)
+
     const [item, setItem] = useState({
                 id:null,
                 title:'',
@@ -84,6 +85,12 @@ const EditItemRoute = (props)=>{
         e.preventDefault()
         let form_data = new FormData();
         form_data.append('img', editItem.img)
+        form_data.append('title', editItem.title)
+        form_data.append('category', editItem.category)
+        form_data.append('details', editItem.details)
+        form_data.append('amount', editItem.amount)
+        form_data.append('location', editItem.location)
+        form_data.append('available', editItem.available)
         let url = `${apiUrl}/api/items/${id}`;
         axios.put(url, form_data, {
             headers:{
@@ -94,9 +101,9 @@ const EditItemRoute = (props)=>{
         .then(res=>{
             console.log(res.data)
             const edited = res.data
-            // const newArray = props.items.map(item => item.id === id ? {...editItem, edited} : item)
-            //     props.setItems(newArray)
-                // navigate (`/items/update/${id}`)
+            const newArray = props.items.map(item => item.id === id ? {...editItem, edited} : item)
+                props.setItems(newArray)
+                navigate (`/items/update/${id}`)
         })
         .catch(err => console.log(err))
        
@@ -104,9 +111,9 @@ const EditItemRoute = (props)=>{
   
     return(
         <div>
-        <div>
-            <h1 id="edit-title">edit <strong>{editItem.title}</strong> here:</h1>
-        </div>
+            <div>
+                <h1 id="edit-title">edit <strong>{editItem.title}</strong> here:</h1>
+            </div>
              <div id="edit-form">
               <form onSubmit={submitEdit} encType="multipart/form"> 
                     <div>
@@ -148,14 +155,43 @@ const EditItemRoute = (props)=>{
                   <Modal show={show} onHide={toggleShow}>
                           <Modal.Header closeButton><Modal.Title>Edit Image</Modal.Title></Modal.Header>
                           
-
-                          
                           <form onSubmit={submitImage} encType="multipart/form">
-                            <div>
-                                <label htmlFor="img">Photo </label>
-                                <input onChange={imageChange} type="file" name="img" id="item-pic"accept="image/png, image/jpeg" placeholder='upload image'></input>
-                            </div>
-                          <button type="submit"onClick={toggleShow}>Submit</button>
+                          <div>
+                        <label htmlFor="img">Photo </label>
+                         <input onChange={imageChange} type="file" name="img" id="item-pic"accept="image/png, image/jpeg" placeholder='upload image'></input>
+                    </div>
+                    <button type="submit"onClick={toggleShow}>Submit</button>
+
+
+                    <div id="hidden">
+                         
+                        <label htmlFor="title">Item(s): </label>
+                        <input onChange={inputChange} type="hidden" name="title" value={editItem.title}></input>
+                    
+                        <label htmlFor="category">Category: </label>
+                        <input onChange={inputChange} type="hidden" name="category" value={editItem.category}></input>
+                   
+                   
+                        <label htmlFor="details">Description: </label>
+                        <input onChange={inputChange} type="hidden" name="details" value={editItem.details}></input>
+                       
+                
+                        <label htmlFor="amount">Amount: </label>
+                        <input onChange={inputChange} type="hidden" name="amount" value={editItem.amount}></input>
+                 
+                    
+                    
+                        <label htmlFor="location">Location: </label>
+                        <input onChange={inputChange} type="hidden" name="location" value={editItem.location}></input>
+                   
+                        <label htmlFor="available">Available: </label>
+                        <input onChange={inputChange} type="hidden" name="available" value={editItem.available}></input>
+     
+                        <label htmlFor="available">Not Available: </label>
+                        <input onChange={inputChange} type="hidden" name="available" value={!editItem.available}></input>
+                    </div>
+                                
+                                
 
                           </form>
                           
